948de1579fc19e2749c86e5cea0f69c48f1c5aa6
Jorge <jbarretolarrosa@gmail.com>
2025-03-28 19:58:41 -0300

    fix(githubconfigapi): erase installationid harshcode

--- src/api/github/github.model.ts
+++ src/api/github/github.model.ts
@@ -3,78 +3,74 @@ import { v4 as uuidv4 } from "uuid";
 import sequelize from "../../config/database";
 import { encodeHashId } from "../helpers/hashid";
 
-class GitHubConfig extends Model<InferAttributes<GitHubConfig, { omit: "hashId" | "projectHashId" | "installationHashId" }>, InferCreationAttributes<GitHubConfig, { omit: "id" | "installationHashId" | "createdAt" | "updatedAt" | "hashId" | "projectHashId" }>> {
-	declare id: string;
-	declare installationId: string;
-	declare repo: string;
-	declare owner: string;
-	declare author: string;
-	declare createdAt: Date;
-	declare updatedAt: Date;
-	declare projectId: string;
+class GitHubConfig extends Model<InferAttributes<GitHubConfig, { omit: "hashId" | "projectHashId" }>, InferCreationAttributes<GitHubConfig, { omit: "id" | "createdAt" | "updatedAt" | "hashId" | "projectHashId" }>> {
+    declare id: string;
+    declare installationId: string;
+    declare repo: string;
+    declare owner: string;
+    declare author: string;
+    declare createdAt: Date;
+    declare updatedAt: Date;
+    declare projectId: string;
 
-	get projectHashId(): string {
-		return encodeHashId(this.projectId);
-	}
+    get projectHashId(): string {
+        return encodeHashId(this.projectId);
+    }
 
-	get hashId(): string {
-		return encodeHashId(this.id);
-	}
-
-	get installationHashId(): string {
-		return encodeHashId(this.installationId);
-	}
+    get hashId(): string {
+        return encodeHashId(this.id);
+    }
 }
 
 GitHubConfig.init(
-	{
-		id: {
-			type: DataTypes.UUID,
-			primaryKey: true,
-			defaultValue: () => uuidv4(),
-		},
-		installationId: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		repo: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		owner: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		author: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		createdAt: {
-			type: DataTypes.DATE,
-			allowNull: false,
-			defaultValue: DataTypes.NOW,
-		},
-		updatedAt: {
-			type: DataTypes.DATE,
-			allowNull: false,
-			defaultValue: DataTypes.NOW,
-		},
-		projectId: {
-			type: DataTypes.UUID,
-			allowNull: false,
-			onDelete: "CASCADE",
-			unique: true,
-			references: {
-				model: "projects",
-				key: "id",
-			},
-		},
-	},
-	{
-		sequelize,
-		tableName: "github-config",
-		timestamps: true,
-	}
+    {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: () => uuidv4(),
+        },
+        installationId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        repo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        owner: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        projectId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            onDelete: "CASCADE",
+            unique: true,
+            references: {
+                model: "projects",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        tableName: "github-config",
+        timestamps: true,
+    }
 );
 
 export default GitHubConfig;
--- src/api/github/github.serializer.ts
+++ src/api/github/github.serializer.ts
@@ -1,19 +1,19 @@
 import GitHubConfig from "./github.model";
 import { InferAttributes } from "sequelize";
 
-type SerializedGitHubConfig = InferAttributes<GitHubConfig, { omit: "hashId" | "projectHashId" | "installationHashId" }>;
+type SerializedGitHubConfig = InferAttributes<GitHubConfig, { omit: "hashId" | "projectHashId" }>;
 
 export const githubSerializer = (input: GitHubConfig | GitHubConfig[]): SerializedGitHubConfig | SerializedGitHubConfig[] => {
-	const serialize = (github: GitHubConfig): SerializedGitHubConfig => ({
-		id: github.hashId,
-		projectId: github.projectHashId,
-		owner: github.owner,
-		repo: github.repo,
-		installationId: github.installationHashId,
-		author: github.author,
-		createdAt: github.createdAt,
-		updatedAt: github.updatedAt,
-	});
+    const serialize = (github: GitHubConfig): SerializedGitHubConfig => ({
+        id: github.hashId,
+        projectId: github.projectHashId,
+        owner: github.owner,
+        repo: github.repo,
+        installationId: github.installationId,
+        author: github.author,
+        createdAt: github.createdAt,
+        updatedAt: github.updatedAt,
+    });
 
-	return Array.isArray(input) ? input.map(serialize) : serialize(input);
+    return Array.isArray(input) ? input.map(serialize) : serialize(input);
 };
