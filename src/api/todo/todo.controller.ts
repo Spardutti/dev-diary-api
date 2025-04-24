cc10119dd8e7960e0bbd927f156cc336daa7bd71
Jorge <jbarretolarrosa@gmail.com>
2025-04-24 16:18:21 -0300

    feat(todos): rework model to use past dates

--- src/api/todo/todo.controller.ts
+++ src/api/todo/todo.controller.ts
@@ -6,77 +6,84 @@ import { decodeHashId } from "../helpers/hashid";
 import { parseQueryParams } from "../helpers/parseQueryParams";
 
 const create = async (req: Request, res: Response): Promise<any> => {
-	try {
-		const { description, title, projectId, priority } = req.body;
+    try {
+        const { description, title, projectId, priority, status, createdAt: created, completedAt: completed } = req.body;
+        const { createdAt, completedAt } = getDates({ created, completed });
+        const todo = await Todo.create({ description, title, projectId: decodeHashId(projectId), status, priority, createdAt, completedAt });
 
-		const todo = await Todo.create({ description, title, projectId: decodeHashId(projectId), status: false, priority });
-
-		res.status(201).json(createResponse(201, todoSerializer(todo)));
-	} catch (error) {
-		res.status(500).json({ message: "Failed to create todo", error });
-	}
+        res.status(201).json(createResponse(201, todoSerializer(todo)));
+    } catch (error) {
+        res.status(500).json({ message: "Failed to create todo", error });
+    }
 };
 
 const list = async (req: Request, res: Response): Promise<any> => {
-	try {
-		const { filters, order } = parseQueryParams(req.query);
+    try {
+        const { filters, order } = parseQueryParams(req.query);
 
-		const { pagination } = req;
+        const { pagination } = req;
 
-		const todos = await Todo.findAll({
-			where: filters,
-			order,
-			limit: pagination?.limit,
-			offset: pagination?.skip,
-		});
+        const todos = await Todo.findAll({
+            where: filters,
+            order,
+            limit: pagination?.limit,
+            offset: pagination?.skip,
+        });
 
-		return res.status(200).json(createResponse(200, todoSerializer(todos), pagination));
-	} catch (error) {
-		return res.status(500).json({ error: "Failed to filter todos" });
-	}
+        return res.status(200).json(createResponse(200, todoSerializer(todos), pagination));
+    } catch (error) {
+        return res.status(500).json({ error: "Failed to filter todos" });
+    }
 };
 
 const update = async (req: Request, res: Response): Promise<any> => {
-	try {
-		const { id } = req.params;
-		const { status, description, title, priority } = req.body;
+    try {
+        const { id } = req.params;
+        const { status, description, title, priority, createdAt: created, completedAt: completed } = req.body;
+        const { createdAt, completedAt } = getDates({ created, completed });
 
-		const todo = await Todo.findByPk(decodeHashId(id));
+        const todo = await Todo.findByPk(decodeHashId(id));
 
-		if (!todo) {
-			return res.status(404).json({ error: "Todo not found" });
-		}
-		const completedAt = status ? new Date() : null;
+        if (!todo) {
+            return res.status(404).json({ error: "Todo not found" });
+        }
 
-		await todo.update({ status, description, title, priority, completedAt });
+        await todo.update({ status, description, title, priority, createdAt, completedAt });
 
-		res.status(200).json(createResponse(200, todoSerializer(todo)));
-	} catch (error) {
-		res.status(500).json({ error: "Failed to update todo" });
-	}
+        res.status(200).json(createResponse(200, todoSerializer(todo)));
+    } catch (error) {
+        res.status(500).json({ error: "Failed to update todo" });
+    }
 };
 
 const remove = async (req: Request, res: Response): Promise<any> => {
-	try {
-		const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-		const todo = await Todo.findByPk(decodeHashId(id));
+        const todo = await Todo.findByPk(decodeHashId(id));
 
-		if (!todo) {
-			return res.status(404).json({ error: "Todo not found" });
-		}
+        if (!todo) {
+            return res.status(404).json({ error: "Todo not found" });
+        }
 
-		await todo.destroy();
+        await todo.destroy();
 
-		res.status(200).json(createResponse(200));
-	} catch (error) {
-		res.status(500).json({ error: "Failed to delete todo" });
-	}
+        res.status(200).json(createResponse(200));
+    } catch (error) {
+        res.status(500).json({ error: "Failed to delete todo" });
+    }
+};
+
+const getDates = ({ created, completed }: { created: string; completed: string }): { createdAt: Date; completedAt: Date | null } => {
+    return {
+        createdAt: created ? new Date(created) : new Date(),
+        completedAt: completed ? new Date(completed) : null,
+    };
 };
 
 export const todoController = {
-	create,
-	list,
-	update,
-	remove,
+    create,
+    list,
+    update,
+    remove,
 };
--- src/api/todo/todo.model.ts
+++ src/api/todo/todo.model.ts
@@ -4,85 +4,85 @@ import sequelize from "../../config/database";
 import { encodeHashId } from "../helpers/hashid";
 
 export enum TodoPriority {
-	Default = 0,
-	Low = 1,
-	Medium = 2,
-	High = 3,
+    Default = 0,
+    Low = 1,
+    Medium = 2,
+    High = 3,
 }
 
-class Todo extends Model<InferAttributes<Todo, { omit: "projectHashId" | "hashId" }>, InferCreationAttributes<Todo, { omit: "id" | "projectHashId" | "createdAt" | "updatedAt" | "hashId" }>> {
-	declare id: string;
-	declare title: string;
-	declare description: string;
-	declare projectId: string;
-	declare status: boolean;
-	declare priority: number;
-	declare completedAt: Date | null;
-	declare createdAt: Date;
-	declare updatedAt: Date;
+class Todo extends Model<InferAttributes<Todo, { omit: "projectHashId" | "hashId" }>, InferCreationAttributes<Todo, { omit: "id" | "projectHashId" | "updatedAt" | "hashId" }>> {
+    declare id: string;
+    declare title: string;
+    declare description: string;
+    declare projectId: string;
+    declare status: boolean;
+    declare priority: number;
+    declare completedAt: Date | null;
+    declare createdAt: Date;
+    declare updatedAt: Date;
 
-	get projectHashId(): string {
-		return encodeHashId(this.projectId);
-	}
+    get projectHashId(): string {
+        return encodeHashId(this.projectId);
+    }
 
-	get hashId(): string {
-		return encodeHashId(this.id);
-	}
+    get hashId(): string {
+        return encodeHashId(this.id);
+    }
 }
 
 Todo.init(
-	{
-		id: {
-			type: DataTypes.UUID,
-			defaultValue: uuidv4,
-			primaryKey: true,
-		},
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		description: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		projectId: {
-			type: DataTypes.UUID,
-			allowNull: false,
-			onDelete: "CASCADE",
-			references: {
-				model: "projects",
-				key: "id",
-			},
-		},
-		status: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false,
-		},
-		priority: {
-			type: DataTypes.INTEGER,
-			defaultValue: TodoPriority.Default,
-			validate: {
-				isIn: [[0, 1, 2, 3]],
-			},
-		},
-		completedAt: {
-			type: DataTypes.DATE,
-			allowNull: true,
-		},
-		createdAt: {
-			type: DataTypes.DATE,
-			defaultValue: DataTypes.NOW,
-		},
-		updatedAt: {
-			type: DataTypes.DATE,
-			defaultValue: DataTypes.NOW,
-		},
-	},
-	{
-		sequelize,
-		tableName: "todos",
-		timestamps: true,
-	}
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: uuidv4,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        projectId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            onDelete: "CASCADE",
+            references: {
+                model: "projects",
+                key: "id",
+            },
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        priority: {
+            type: DataTypes.INTEGER,
+            defaultValue: TodoPriority.Default,
+            validate: {
+                isIn: [[0, 1, 2, 3]],
+            },
+        },
+        completedAt: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+    },
+    {
+        sequelize,
+        tableName: "todos",
+        timestamps: true,
+    }
 );
 
 export default Todo;
